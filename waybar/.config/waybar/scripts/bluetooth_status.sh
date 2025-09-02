f9c52c82a3a14fc124e10a9c36d88b3bdcc1e63c
Henil Malaviya <henilm.dev@gmail.com>
2025-09-02 13:34:57 +0530

    feat: show multiple bluetooth connected devices

--- waybar/.config/waybar/scripts/bluetooth_status.sh
+++ waybar/.config/waybar/scripts/bluetooth_status.sh
@@ -1,25 +1,38 @@
 #!/usr/bin/env bash
 
 ICON=""
+MAX_LENGTH=10
 
-CONNECTED_DEVICE=$(bluetoothctl info | grep "Name" | awk -F ': ' '{print $2}')
+# Get all connected devices (MAC addresses)
+CONNECTED_DEVICES=$(bluetoothctl devices | awk '{print $2}' | grep -E '^([0-9A-F]{2}:){5}[0-9A-F]{2}$')
 
-BATTERY_RAW=$(bluetoothctl info | grep "Battery Percentage" | grep -oP '\(\K[0-9]+')
+DISPLAY_TEXT="$ICON"
 
-if [ -n "$BATTERY_RAW" ]; then
-    BATTERY_TEXT="${BATTERY_RAW}%"
-else
-    BATTERY_TEXT=""
-fi
-
-if [ -z "$CONNECTED_DEVICE" ]; then
+if [ -z "$CONNECTED_DEVICES" ]; then
     echo "{\"text\": \"$ICON\", \"tooltip\": \"No device connected\"}"
 else
-    MAX_LENGTH=10
-    if [ ${#CONNECTED_DEVICE} -gt $MAX_LENGTH ]; then
-        SHORT_NAME="${CONNECTED_DEVICE:0:$MAX_LENGTH}…"
-    else
-        SHORT_NAME="$CONNECTED_DEVICE"
-    fi
-    echo "{\"text\": \"$ICON  $SHORT_NAME ($BATTERY_TEXT)\", \"tooltip\": \"Connected to $CONNECTED_DEVICE\"}"
+    DEVICES_ARRAY=()
+    TOOLTIP_ARRAY=()
+    for MAC in $CONNECTED_DEVICES; do
+        INFO=$(bluetoothctl info "$MAC")
+        NAME=$(echo "$INFO" | grep "Name" | awk -F ': ' '{print $2}')
+        BATTERY=$(echo "$INFO" | grep "Battery Percentage" | grep -oP '\(\K[0-9]+')
+        [ ${#NAME} -gt $MAX_LENGTH ] && SHORT_NAME="${NAME:0:$MAX_LENGTH}…" || SHORT_NAME="$NAME"
+        if [ -n "$BATTERY" ]; then
+            DEVICES_ARRAY+=("$SHORT_NAME (${BATTERY}%)")
+            TOOLTIP_ARRAY+=("$NAME (${BATTERY}%)")
+        else
+            DEVICES_ARRAY+=("$SHORT_NAME")
+            TOOLTIP_ARRAY+=("$NAME")
+        fi
+    done
+
+    FINAL_TEXT="$ICON  $(printf '%s, ' "${DEVICES_ARRAY[@]}")"
+    FINAL_TEXT="${FINAL_TEXT%, }"  # Remove trailing comma and space
+
+    FINAL_TOOLTIP="$(printf '%s\\n' "${TOOLTIP_ARRAY[@]}")"
+    FINAL_TOOLTIP="${FINAL_TOOLTIP%\\n}"  # Remove trailing newline for cleaner output
+
+    echo "{\"text\": \"$FINAL_TEXT\", \"tooltip\": \"$FINAL_TOOLTIP\"}"
+
 fi
