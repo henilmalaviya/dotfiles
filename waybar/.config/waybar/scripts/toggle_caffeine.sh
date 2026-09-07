eba34fe20e09025841744d0777986786a5f89d58
Henil Malaviya <henilm.dev@gmail.com>
2026-09-07 15:00:51 +0530

    refactor(hypridle): simplify idle timeouts and control service in caffeine toggle

--- waybar/.config/waybar/scripts/toggle_caffeine.sh
+++ waybar/.config/waybar/scripts/toggle_caffeine.sh
@@ -3,11 +3,12 @@
 state_file="/tmp/waybar_caffeine_state"
 
 if [[ -f "$state_file" ]]; then
-    # Disable caffeine (restart hypridle)
-    hypridle &
+    # Disable caffeine (restart hypridle service)
+    systemctl --user start dotfiles-hypridle.service
     rm "$state_file"
 else
-    # Enable caffeine (kill hypridle)
-    pkill hypridle
+    # Enable caffeine (stop hypridle service)
+    systemctl --user stop dotfiles-hypridle.service
+    pkill -9 hypridle 2>/dev/null
     touch "$state_file"
 fi
